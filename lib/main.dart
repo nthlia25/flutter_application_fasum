1f37cc96ba8c27ade173ee90671728f9bc39cfe5
nthlia25 <nathaliamargaretha2506@gmail.com>
2026-05-19 19:57:14 +0700

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_fasum/firebase_options.dart';
 import 'package:flutter_application_fasum/screens/sign_in_screen.dart';
 import 'package:flutter_application_fasum/screens/sign_up_screen.dart';
-
-
-
+import 'package:flutter_application_fasum/screens/splash_screen.dart';
 
 Future<void> main() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
- 
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
 
   // This widget is the root of your application.
   @override
@@ -30,7 +24,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      home: const SignInScreen(),
+      home: const SplashScreen(),
     );
   }
 }
